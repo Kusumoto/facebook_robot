1c2dae23ed26a57c327b6a88b38953748a19dd4f
Kusumoto <kusumoto.com@gmail.com>
2013-01-13 13:41:07 +0700

    Add Backup table and System Backup

---
+++ backup_db.php
@@ -0,0 +1,64 @@
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+<title>Micro Facebook Robot - Backup</title>
+<link rel="shortcut icon" type="image/x-icon" href="http://data.kusumoto.co/logo.png">
+<?php
+	  include("sql_setting.php");
+      
+	  mysql_connect($sql_host,$sql_user,$sql_pass)or die("Error SQL Server");
+      mysql_select_db("$sql_db");
+      mysql_query("set names utf8");
+       $query = mysql_query("SELECT * FROM `facebook_status` ORDER BY id DESC") or die(mysql_error());
+       $result = mysql_num_rows($query);
+       echo "<b><font size='4'>Backup Status...</font></b></br>";
+       $i=0;
+        while( $i < $result ) {
+    $fetcharr1 = mysql_fetch_array($query);
+	$fb_id = $fetcharr1['fb_id'];
+	$message = $fetcharr1['message'];
+	$story = $fetcharr1['story'];
+	$from_name = $fetcharr1['from_name'];
+	$from_id = $fetcharr1['from_id'];
+	$type = $fetcharr1['type'];
+	$created_time = $fetcharr1['created_time'];
+	$application = $fetcharr1['application'];
+		$query_status = mysql_query("SELECT * FROM `facebook_logstatus` WHERE fb_id='$fb_id'") or die(mysql_error());
+        $result_status = mysql_num_rows($query_status);
+        if (!empty($result_status)) {
+         echo "Ignore post $fb_id complete!</br>";
+        } else {
+	$query1 = mysql_query(sprintf("INSERT INTO `facebook_logstatus` (fb_id, message, story, from_name, from_id, type, created_time, application) VALUES ('$fb_id', '$message', '$story', '$from_name', '$from_id', '$type', '$created_time', '$application')"))or die(mysql_error());	
+    echo "backup post $fb_id complete!</br>";
+    	}
+    $i++;
+    }
+    
+    $query = mysql_query("SELECT * FROM `facebook_photo` ORDER BY id DESC") or die(mysql_error());
+    $result = mysql_num_rows($query);
+    echo "<b><font size='4'>Backup photo...</font></b></br>";
+     $y=0;
+        while( $y < $result ) {
+    $fetcharr1 = mysql_fetch_array($query);
+	$fb_id = $fetcharr1['fb_id'];
+	$message = $fetcharr1['message'];
+	$like = $fetcharr1['like'];
+	$picture = $fetcharr1['picture'];
+	$original_link = $fetcharr1['original_link'];
+	$created_time = $fetcharr1['created_time'];
+	$fb_user = $fetcharr1['fb_user'];
+	$query_photo = mysql_query("SELECT * FROM `facebook_logphoto` WHERE fb_id='$fb_id'") or die(mysql_error());
+        $result_photo = mysql_num_rows($query_photo);
+        if (!empty($result_status)) {
+         echo "Ignore photo $fb_id complete!</br>";
+        } else {
+	$query2 = mysql_query("INSERT INTO `facebook_logphoto`(`fb_id`, `message`, `like`, `picture`, `original_link`, `created_time`, `fb_user`) VALUES ('$fb_id','$message','$like','$picture','$original_link','$created_time','$from_id')") or die(mysql_error());
+	 echo "backup photo $fb_id complete!</br>";
+	 }
+    $y++;
+    }
+     	 $query = mysql_query("TRUNCATE `facebook_status` ") or die(mysql_error());
+     	 $query = mysql_query("TRUNCATE `facebook_photo` ") or die(mysql_error());
+     	 echo "</br><b><font size='4'>Cleanup Complete</font></b>";
+?>
+    
+	
+    
--- sql_install.sql
+++ sql_install.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 12, 2013 at 06:42 AM
+-- Generation Time: Jan 13, 2013 at 01:36 AM
 -- Server version: 5.5.14
 -- PHP Version: 5.2.17
 
@@ -23,20 +23,57 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `facebook_logphoto`
+--
+
+CREATE TABLE IF NOT EXISTS `facebook_logphoto` (
+  `id` int(50) NOT NULL AUTO_INCREMENT,
+  `fb_id` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `fb_user` text COLLATE utf8_unicode_ci NOT NULL,
+  `message` text COLLATE utf8_unicode_ci NOT NULL,
+  `like` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `picture` text COLLATE utf8_unicode_ci NOT NULL,
+  `original_link` text COLLATE utf8_unicode_ci NOT NULL,
+  `created_time` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=9 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `facebook_logstatus`
+--
+
+CREATE TABLE IF NOT EXISTS `facebook_logstatus` (
+  `id` int(50) NOT NULL AUTO_INCREMENT,
+  `fb_id` varchar(40) CHARACTER SET latin1 NOT NULL,
+  `message` text CHARACTER SET utf32 COLLATE utf32_unicode_ci NOT NULL,
+  `story` text CHARACTER SET utf32 COLLATE utf32_unicode_ci NOT NULL,
+  `from_name` text CHARACTER SET utf16 COLLATE utf16_unicode_ci NOT NULL,
+  `from_id` varchar(40) CHARACTER SET latin1 NOT NULL,
+  `type` varchar(20) CHARACTER SET utf16 COLLATE utf16_unicode_ci NOT NULL,
+  `created_time` varchar(20) CHARACTER SET utf16 COLLATE utf16_unicode_ci NOT NULL,
+  `application` varchar(30) CHARACTER SET utf32 COLLATE utf32_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=26 ;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `facebook_photo`
 --
 
 CREATE TABLE IF NOT EXISTS `facebook_photo` (
   `id` int(50) NOT NULL AUTO_INCREMENT,
-  `fb_id` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `fb_user` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `message` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `like` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `picture` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `original_link` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `created_time` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `fb_id` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `fb_user` text COLLATE utf8_unicode_ci NOT NULL,
+  `message` text COLLATE utf8_unicode_ci NOT NULL,
+  `like` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `picture` text COLLATE utf8_unicode_ci NOT NULL,
+  `original_link` text COLLATE utf8_unicode_ci NOT NULL,
+  `created_time` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=478 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=13 ;
 
 -- --------------------------------------------------------
 
@@ -55,7 +92,7 @@ CREATE TABLE IF NOT EXISTS `facebook_status` (
   `created_time` varchar(20) CHARACTER SET utf16 COLLATE utf16_unicode_ci NOT NULL,
   `application` varchar(30) CHARACTER SET utf32 COLLATE utf32_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3256 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=34 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
